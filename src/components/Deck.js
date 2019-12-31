6d73a6742b498fc29217dc6c01c04eacfa6b52a9
Yudai Watanabe <lock0308@gmail.com>
2020-01-01 01:15:17 +0900

    Deck コンポーネント作成

---
+++ src/components/Deck.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: "relative",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(10),
+      height: theme.spacing(14),
+      position: "absolute"
+    },
+  },
+}));
+
+export default function SimplePaper() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Paper style={{zIndex: 5, top: 0}} elevation={3} />
+      <Paper style={{zIndex: 4, top: 2}} elevation={3} />
+      <Paper style={{zIndex: 3, top: 4}} elevation={3} />
+      <Paper style={{zIndex: 2, top: 6}} elevation={3} />
+      <Paper style={{zIndex: 1, top: 8}} elevation={3} />
+      <Paper style={{zIndex: 0, top: 10}} elevation={3} />
+    </div>
+  );
+}
