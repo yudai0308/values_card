6b3c704900ae100c69b1628e0a66a1577251c4cf
Yudai Watanabe <lock0308@gmail.com>
2019-12-31 17:34:16 +0900

    メニュー作成

--- src/components/Toppage.js
+++ src/components/Toppage.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Link } from 'react-router-dom'
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 
 export default function Toppage() {
   return (
-    <Typography component="div">
-      <Link to="/single">Single</Link>
-      TOPPAGE
+    <Typography component="div" variant="h4">
+      <Box
+        height={500}
+        display="flex"
+        flexDirection="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Box m={2}><Link style={{color: "lightgrey"}} to="/single">ひとりで遊ぶ</Link></Box>
+        <Box m={2}><Link style={{color: "lightgrey"}} to="/room">みんなで遊ぶ</Link></Box>
+        <Box m={2}><Link style={{color: "lightgrey"}} to="/about">遊び方</Link></Box>
+      </Box>
     </Typography>
   );
 }
