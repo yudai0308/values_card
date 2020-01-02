cea08c09771918eefbd90b47befdde7d4af652ca
Yudai Watanabe <lock0308@gmail.com>
2020-01-03 00:08:44 +0900

    libs.js 作成

---
+++ src/libs.js
@@ -0,0 +1,9 @@
+export const shuffle = array => {
+  for(var i = array.length - 1; i > 0; i--){
+    var r = Math.floor(Math.random() * (i + 1));
+    var tmp = array[i];
+    array[i] = array[r];
+    array[r] = tmp;
+  }
+  return array;
+}
