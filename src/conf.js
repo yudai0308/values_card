943fd6539f5fd6c97dc489e0652087954020d93d
Yudai Watanabe <lock0308@gmail.com>
2020-01-03 16:18:28 +0900

    デフォルトの gameState と playerState を作成

--- src/conf.js
+++ src/conf.js
@@ -1,5 +1,20 @@
-export const field = {
+export const fieldFrame = {
   width: 800,
   height: 600,
   sideWidth: 100
-}
+};
+
+export const defaultGameState = {
+  wasStarted: false,
+  wasEnded: false,
+  deck: [],
+  discards: [],
+  players: [],
+};
+
+export const defaultPlayerState = {
+  id: null,
+  name: "",
+  hand: [],
+  drew: { id: null, name: "" },
+};
