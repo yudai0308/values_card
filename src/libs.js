532e867cf8b7b7b2da84a5aacc11bc6572c8b212
Yudai Watanabe <lock0308@gmail.com>
2020-01-05 23:14:27 +0900

    var を let に変更

--- src/libs.js
+++ src/libs.js
@@ -1,7 +1,7 @@
 export const shuffle = array => {
-  for(var i = array.length - 1; i > 0; i--){
-    var r = Math.floor(Math.random() * (i + 1));
-    var tmp = array[i];
+  for (let i = array.length - 1; i > 0; i--) {
+    let r = Math.floor(Math.random() * (i + 1));
+    let tmp = array[i];
     array[i] = array[r];
     array[r] = tmp;
   }
