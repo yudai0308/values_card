c244087fcaba0519c83147ac7774b73db38d7454
Yudai Watanabe <lock0308@gmail.com>
2020-01-18 16:16:35 +0900

    不要なコメントアウト削除

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
-var firebaseConfig = {
+const firebaseConfig = {
   "projectId": "values-card-test",
   "appId": "1:379819919241:web:0a925d23af1e70b9a28358",
   "databaseURL": "https://values-card-test.firebaseio.com",
@@ -13,8 +13,7 @@ var firebaseConfig = {
   "messagingSenderId": "379819919241",
   "measurementId": "G-4KDMM2TVT0"
 };
-// if (firebaseConfig) {
-  const firebaseApp = firebase.initializeApp(firebaseConfig);
-// }
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export const db = firebaseApp.firestore();
