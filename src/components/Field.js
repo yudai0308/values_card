8e78ff17ba40806364368d12f27a246caecb441c
Yudai Watanabe <lock0308@gmail.com>
2020-01-18 16:16:48 +0900

    インデント調整

--- src/components/Field.js
+++ src/components/Field.js
@@ -12,8 +12,8 @@ export default function Field(props) {
     <GameContext.Provider value={{ gameState: gameState, gameDispatch: gameDispatch }}>
       {
         !gameState.ended
-        ? <CardTable props={props} />
-        : <ResultPage />
+          ? <CardTable props={props} />
+          : <ResultPage />
       }
     </GameContext.Provider>
   );
